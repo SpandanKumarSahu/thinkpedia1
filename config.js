cc061f3952bff6f3c2ac079d64ad5ad5180967ae
saurabhsinghola <OLA@Saurabhs-MBP.connectify>
2016-09-03 12:03:51 +0530

    letsyudy
    Copyright 2016

---
+++ config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by digvijay on 11/12/15.
+ */
+module.exports = {
+
+    'secret': 'ilovescotchyscotch',
+    //'database': 'mongodb://noder:noderauth&54;proximus.modulusmongo.net:27017/so9pojyN'
+
+};
